09e12386326390d96a960993521150262f2a2ff9
Burke Ramsey <birk5437@gmail.com>
2016-05-12 19:57:45 -0400

    first commit!

--- HappyHour/ViewController.swift
+++ HappyHour/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "Test"
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
