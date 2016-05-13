0391283c18bb1536a7b15e840f684c18064c350b
Burke Ramsey <birk5437@gmail.com>
2016-05-12 21:14:50 -0400

    can now get data from the web

--- HappyHour/ViewController.swift
+++ HappyHour/ViewController.swift
@@ -9,19 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate {
+    
+    var cellContent = ["Bridge Street Tap Room",
+                       "Lake Charlevoix Brewing Company",
+                       "Townhouse Bar",
+                       "Tap 30",
+                       "City Park Grill",
+                       "Noggin Room",
+                       "Mitchell Street Pub"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let url = NSURL(string: "http://burkeramsey.com")!
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url){ (data, response, error) -> Void in
+            if let urlContent = data {
+                // https://www.udemy.com/the-complete-ios-9-developer-course/learn/v4/t/lecture/3193384
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    print(webContent)
+                    // do something with data
+                })
+            
+            } else {
+                // Show error
+            }
+        }
+        task.resume()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return cellContent.count
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
-        cell.textLabel?.text = "Test"
+        cell.textLabel?.text = cellContent[indexPath.row]
         return cell
     }
 
